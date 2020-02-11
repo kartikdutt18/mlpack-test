ce63e9605b07741ccd3a1302bed1805561f54680
kartikdutt18 <kartikdutt@live.in>
2020-02-11 19:45:12 +0530

    Pooling Tests

--- Pooling/pooling-test.cpp
+++ Pooling/pooling-test.cpp
@@ -9,19 +9,14 @@ int main()
 {
   arma::mat input = arma::mat(12, 1);
   arma::mat output;
+  input = arma::mat(6, 1);
   input.zeros();
   input(0) = 1;
-  input(1) = 2;
-  input(2) = 3;
-  input(3) = input(8) = 7;
-  input(4) = 4;
-  input(5) = 5;
-  input(6) = input(7) = 6;
-  input(10) = 8;
-  input(11) = 9;
-  MeanPooling<> layer1(2, 2, 2, 1);
-  layer1.InputHeight() = 3;
-  layer1.InputWidth() = 4;
+  input(1) = 1;
+  input(3) = 1;
+  MaxPooling<> layer1(2, 1, 1, 1, true);
+  layer1.InputHeight() = 2;
+  layer1.InputWidth() = 3;
   layer1.Forward(std::move(input), std::move(output));
   input.print();
   cout << "-------" << endl;
